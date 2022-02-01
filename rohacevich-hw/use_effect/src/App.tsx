ca5424b0a9a0e501e0beff887ed28b25f08ce822
Mkaell <mrohacevich@yandex.ru>
2022-02-01 20:00:39 +0300

    added first version of the app

--- rohacevich-hw/use_effect/src/App.tsx
+++ rohacevich-hw/use_effect/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import { Github } from './components/Github';
 
 function App() {
   return (
     <div className="App">
-
+      <Github />
     </div>
   );
 }
